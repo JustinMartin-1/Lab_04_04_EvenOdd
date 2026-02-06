56732e63afeb784e516f1d10eeafaa87adce7d77
Jusitn783 <justin.martin9001@gmail.com>
2026-02-06 12:56:35 -0500

    Complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+public static void main(String[] args) {
+        int numToExamine = 2;
 
-}
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is Even");
+        } else {
+            System.out.println(numToExamine + " is Odd");
+        }
+    }
